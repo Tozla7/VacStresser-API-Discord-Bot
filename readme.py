79248a112eaedb6849ef6d95e74c2717d97c4faf
Tozla7 <131265506+Tozla7@users.noreply.github.com>
2023-12-05 21:21:33 -0500

    Update and rename README.md to readme.py

---
+++ readme.py
@@ -0,0 +1,8 @@
+# VacStresser-API-Discord-Bot
+discord bot that connects to VacStresser API to send attacks from a discord chat.
+
+For access, please contact me through one of the following methods
+  Discord - Tozla7 or TerrenceAndrewDavis_v2
+  Telegram - Tozla7
+  Wire - Tozla7
+
